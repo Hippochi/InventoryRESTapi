651c1500eb08859fc7762b9f7144673cfc03f163
Alex Dine <alexdine1996@gmail.com>
2022-02-17 03:57:48 -0500

    Added Get Put Push and Delete functionality

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Catalog.Entities;
 using System;
-
+using System.Linq;
+using Catalog.Dtos;
 
 namespace Catalog.Controllers
 {
@@ -19,21 +20,78 @@ namespace Catalog.Controllers
             }
 
             [HttpGet]
-            public IEnumerable<Item> GetItems()
+            public IEnumerable<ItemDto> GetItems()
             {
-                var items = repository.GetItems();
+                var items = repository.GetItems().Select( item => item.AsDto());
+
                 return items;
             }
             [HttpGet("{id}")]
-            public ActionResult<Item> GetItem(Guid Id)
+            public ActionResult<ItemDto> GetItem(Guid id)
             {
-                var item = repository.GetItem(Id);
+                var item = repository.GetItem(id);
 
                 if (item is null)
                 {
                     return NotFound();
                 }
-                return item;
+                return item.AsDto();
+            }
+            
+            // POST / items
+            [HttpPost]
+            public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+            {
+                Item item = new(){
+                    Id = Guid.NewGuid(),
+                    Name = itemDto.Name,
+                    Price = itemDto.Price,
+                    CreatedDate = DateTimeOffset.UtcNow
+                };
+
+                repository.CreateItem(item);
+
+                return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
+            }
+
+            //put / items/{id}
+            [HttpPut("{id}")]
+            public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+            {
+                var exisitingItem = repository.GetItem(id);
+
+                if (exisitingItem is null)
+                {
+                    return NotFound();
+                }
+
+                Item updatedItem = exisitingItem with {
+                    Name = itemDto.Name,
+                    Price = itemDto.Price
+                };
+
+                repository.UpdateItem(updatedItem);
+
+                return NoContent();
+
+
+            }
+
+
+            // Delete /items/{id}
+            [HttpDelete("{id}")]
+            public ActionResult DeleteItem(Guid id)
+            {
+                var exisitingItem = repository.GetItem(id);
+
+                if (exisitingItem is null)
+                {
+                    return NotFound();
+                }
+
+                repository.DeleteItem(id);
+
+                return NoContent();
             }
         }  
 }
