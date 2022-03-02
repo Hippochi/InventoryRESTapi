c691543de4327eb9fd5c52b69fa9edf37070bbdc
Alex Dine <alexdine1996@gmail.com>
2022-03-01 20:39:41 -0500

    added health checks + user/pass

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -5,6 +5,7 @@ using Catalog.Entities;
 using System;
 using System.Linq;
 using Catalog.Dtos;
+using System.Threading.Tasks;
 
 namespace Catalog.Controllers
 {
@@ -20,16 +21,17 @@ namespace Catalog.Controllers
             }
 
             [HttpGet]
-            public IEnumerable<ItemDto> GetItems()
+            public async Task<IEnumerable<ItemDto>> GetItemsAsync()
             {
-                var items = repository.GetItems().Select( item => item.AsDto());
+                var items = (await repository.GetItemsAsync())
+                            .Select( item => item.AsDto());
 
                 return items;
             }
             [HttpGet("{id}")]
-            public ActionResult<ItemDto> GetItem(Guid id)
+            public async Task<ActionResult<ItemDto>> GetItemAsync(Guid id)
             {
-                var item = repository.GetItem(id);
+                var item = await repository.GetItemAsync(id);
 
                 if (item is null)
                 {
@@ -40,7 +42,7 @@ namespace Catalog.Controllers
             
             // POST / items
             [HttpPost]
-            public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+            public async Task<ActionResult<ItemDto>> CreateItemAsync(CreateItemDto itemDto)
             {
                 Item item = new(){
                     Id = Guid.NewGuid(),
@@ -49,16 +51,16 @@ namespace Catalog.Controllers
                     CreatedDate = DateTimeOffset.UtcNow
                 };
 
-                repository.CreateItem(item);
+                await repository.CreateItemAsync(item);
 
-                return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
+                return CreatedAtAction(nameof(GetItemAsync), new {id = item.Id}, item.AsDto());
             }
 
             //put / items/{id}
             [HttpPut("{id}")]
-            public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+            public async Task<ActionResult> UpdateItemAsync(Guid id, UpdateItemDto itemDto)
             {
-                var exisitingItem = repository.GetItem(id);
+                var exisitingItem = await repository.GetItemAsync(id);
 
                 if (exisitingItem is null)
                 {
@@ -70,7 +72,7 @@ namespace Catalog.Controllers
                     Price = itemDto.Price
                 };
 
-                repository.UpdateItem(updatedItem);
+                await repository.UpdateItemAsync(updatedItem);
 
                 return NoContent();
 
@@ -80,16 +82,16 @@ namespace Catalog.Controllers
 
             // Delete /items/{id}
             [HttpDelete("{id}")]
-            public ActionResult DeleteItem(Guid id)
+            public async Task<ActionResult> DeleteItemAsync(Guid id)
             {
-                var exisitingItem = repository.GetItem(id);
+                var exisitingItem = await repository.GetItemAsync(id);
 
                 if (exisitingItem is null)
                 {
                     return NotFound();
                 }
 
-                repository.DeleteItem(id);
+                await repository.DeleteItemAsync(id);
 
                 return NoContent();
             }
--- Catalog/obj/Debug/net5.0/Catalog.AssemblyInfo.cs
+++ Catalog/obj/Debug/net5.0/Catalog.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9a9c0621-0a6e-45d9-bc88-b1411bc4f89f")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
